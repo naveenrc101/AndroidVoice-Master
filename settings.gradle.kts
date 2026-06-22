caf216151a299100a94c8bc2877629b3cc2862ce
naveenc101 <nchigadani@concertai.com>
2026-06-22 20:12:03 +0530

    fix

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+// settings.gradle.kts
+// This registry ties the build execution context to our ":app" subfolder target.
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "VoiceCallBouncer"
+include(":app")
